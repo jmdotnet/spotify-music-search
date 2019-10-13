8d5a1bb340ae4ba6c2e2ca066ce542878c44905f
justi <jmaurer@microappbrewery.com>
2019-10-13 16:59:17 -0500

    Add project files.

---
+++ Jmdotnet.MusicSearch.SpotifyWrapper/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Jmdotnet.MusicSearch.SpotifyWrapper
+{
+    public class Class1
+    {
+    }
+}
---
+++ Jmdotnet.MusicSearch.SpotifyWrapper/Infrastructure/Auth/ClientCredentialAuth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Jmdotnet.MusicSearch.SpotifyWrapper.Infrastructure.Auth
+{
+    public class ClientCredentialAuth
+    {
+        public string ClientSecret { get; private set; }
+        public string ClientId { get; private set; }
+
+        public ClientCredentialAuth(string clientId, string clientSecret)
+        {
+            ClientId = clientId;
+            ClientSecret = clientSecret;
+        }
+
+
+    }
+}
